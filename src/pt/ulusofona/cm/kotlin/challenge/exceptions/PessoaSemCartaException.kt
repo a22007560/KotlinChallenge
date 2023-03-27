4e69ccadfbd3678ae05c9dc0049f69bab1964280
a22007560 <a22007560@alunos.ulht.pt>
2023-03-27 15:00:46 +0100

    funciona por favor

--- src/pt/ulusofona/cm/kotlin/challenge/exceptions/PessoaSemCartaException.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/exceptions/PessoaSemCartaException.kt
@@ -1,4 +1,4 @@
 package pt.ulusofona.cm.kotlin.challenge.exceptions
 
-class PessoaSemCartaException(name:String, message: String ="$name não tem carta para conduzir o veículo indicado"):Exception() {
+class PessoaSemCartaException(name:String, message: String ="$name não tem carta para conduzir o veículo indicado"):Exception(message) {
 }
